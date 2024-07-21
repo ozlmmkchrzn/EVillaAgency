223cc2e77e7ead032755eb705d99f1f3d78d75f5
erhanzorlu <zorlu.com@gmail.com>
2024-07-21 23:57:09 +0300

    .

---
+++ EVillaAgency.WebUI/Controllers/Admin/AdminLayoutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EVillaAgency.WebUI.Controllers.Admin
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public IActionResult _AdminLayout()
+		{
+			return View();
+		}
+
+	}
+}
---
+++ EVillaAgency.WebUI/ViewComponents/LayoutComponents/_LayoutFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EVillaAgency.WebUI.ViewComponents.LayoutComponents
+{
+	public class _LayoutFooterComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
