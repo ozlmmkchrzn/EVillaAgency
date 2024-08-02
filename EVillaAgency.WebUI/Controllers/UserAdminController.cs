5754c5d10db8b4a8e95eaca454348ee7a5262eb3
erhanzorlu <zorlu.com@gmail.com>
2024-08-02 15:08:30 +0300

    .

---
+++ EVillaAgency.WebUI/Controllers/UserAdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EVillaAgency.WebUI.Controllers
+{
+    public class UserAdminController : Controller
+    {
+
+        public IActionResult _UserAdminLayout()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
